7c3f3b39b110d29597e2415bf3e88303b5db890e
vil02 <vil02@o2.pl>
2023-10-28 17:17:06 +0200

    fix: add `_prepare_str`

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import os
 from string_to_code import string_to_code
 
 
+def _prepare_str(in_str):
+    res = in_str.replace("%", "%25")
+    res = res.replace("\n", "%0A")
+    return res.replace("\r", "%0D")
+
+
 def _set_output(output_name, output_value):
     with open(
         os.path.abspath(os.environ["GITHUB_OUTPUT"]), mode="a", encoding="utf-8"
     ) as _:
-        _.write(f"{output_name}=\"{output_value}\"")
+        _.write(f"{output_name}=\"{_prepare_str(output_value)}\"")
 
 
 def main():
